d362cc385b126e337df2d2ef0957898bcebed0b9
mckasap <mckasap@gmail.com>
2019-04-26 14:21:04 +0300

    ActionBAR

---
+++ app/src/main/java/org/kasapbasi/week20191201/MainActivity.java
@@ -0,0 +1,13 @@
+package org.kasapbasi.week20191201;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
